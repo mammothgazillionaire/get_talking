11c266cf48aed2f680031b6e70f8498051096af3
Nupur Agrahari <nupur.agrahari93@gmail.com>
2018-03-05 03:22:35 +0530

    config

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,3 +1,4 @@
 module.exports = {
-    uri : 'mongodb+srv://nupuragrahari:nupur1..@gettalkingdev-lmctj.mongodb.net/getTalkingDev?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin'
-};
+    uri : 'mongodb://localhost/local_db'
+
+}
